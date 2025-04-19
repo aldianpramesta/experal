67a490b6c368066ce1a82cae184348beb86ebff0
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-19 10:46:47 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,19 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const email = result.user.email;
+    const allowedEmails = ["aldianpramesta@gmail.com", "pembeli2@gmail.com"];
+    if (allowedEmails.includes(email)) {
+      window.location.href = "experal1.html";
+    } else {
+      alert("Akun ini belum terdaftar sebagai pembeli.");
+      auth.signOut();
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
