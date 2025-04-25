2bb7cc944c4053f39c84e0ab0e9399e86e523d35
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-25 22:12:28 +0700

    Create disable.inspect.js

---
+++ disable.inspect.js
@@ -0,0 +1,10 @@
+// disable-inspect.js
+  document.addEventListener('keydown', function (e) {
+    if (
+      e.key === 'F12' ||
+      (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key)) ||
+      (e.ctrlKey && e.key === 'U')
+    ) {
+      e.preventDefault();
+    }
+  });
