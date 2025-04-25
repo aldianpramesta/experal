51e5205cf9ef3fc5e3d58aa049f68dde2133e920
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-25 21:45:44 +0700

    Update disable-inspect.js

--- javascript/disable-inspect.js
+++ javascript/disable-inspect.js
@@ -1,4 +1,10 @@
 // disable-inspect.js
-  document.addEventListener('contextmenu', function(event) {
-    event.preventDefault(); // Fungsi ini mencegah menu klik kanan muncul
+  document.addEventListener('keydown', function (e) {
+    if (
+      e.key === 'F12' ||
+      (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key)) ||
+      (e.ctrlKey && e.key === 'U')
+    ) {
+      e.preventDefault();
+    }
   });
