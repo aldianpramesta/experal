77fe7d236c60363bff91dbfc88327b1508c4f139
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-25 21:09:47 +0700

    Create video-toggle.js

---
+++ javascript/video-toggle.js
@@ -0,0 +1,5 @@
+// video-toggle.js
+function toggleVideo(id) {
+    const videoEl = document.getElementById(id);
+    videoEl.style.display = videoEl.style.display === 'block' ? 'none' : 'block';
+  }
