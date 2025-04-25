0d67122ab136dc191e11808c3ad6eb0f8e47a2b9
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-25 21:31:49 +0700

    Update disable-inspect.js

--- javascript/disable-inspect.js
+++ javascript/disable-inspect.js
@@ -1,4 +1,4 @@
 // disable-inspect.js
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-});
+  document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Fungsi ini mencegah menu klik kanan muncul
+  });
