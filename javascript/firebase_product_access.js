dceaf51dad8d4b96b396ecf143574d12cf9b8d39
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-26 08:25:13 +0700

    Update firebase_product_access.js

--- javascript/firebase_product_access.js
+++ javascript/firebase_product_access.js
@@ -1,7 +1,7 @@
    // Inisialisasi firebase
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-    import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-    import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+    import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+    import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
     // Konfigurasi firebase project kamu
     const firebaseConfig = {
