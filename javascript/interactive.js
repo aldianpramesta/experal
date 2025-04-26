74b67ff1258ad7af28a31bec7f4f24fdb5ccb8dc
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-26 14:46:00 +0700

    Update interactive.js

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,5 +1,10 @@
-// toggle video
+// toggle pilihan part dari modul
 function toggleVideo(id) {
     const videoEl = document.getElementById(id);
     videoEl.style.display = videoEl.style.display === 'block' ? 'none' : 'block';
+  }
+
+// toggle pilihan akses saat masuk
+  function showOptions() {
+    document.getElementById('options').style.display = 'block';
   }
