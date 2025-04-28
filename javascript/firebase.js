5ba5bd71b27d00c861b8fd16e0d95bcd9dcbfc4c
aldianpramesta <151911799+aldianpramesta@users.noreply.github.com>
2025-04-28 15:56:56 +0700

    mundur 1

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -4,6 +4,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-storage.js";
 
 // Konfigurasi proyek Firebase
 const firebaseConfig = {
@@ -74,3 +75,6 @@ window.loginModul = async function(namaModul) {
 
 // --- wajib paling bawah buat dikirim ke firebase modul ---
 export { auth, db };
+// Export biar bisa dipakai file lain
+export const auth = getAuth(app);
+export const storage = getStorage(app);
